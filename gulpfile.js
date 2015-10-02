bbb364adb07e73d1b498adca691a3fbeeb46f03f
Ruby Schmidmeister <ruby@fog.im>
2015-10-02 22:05:22 +0200

    prepare sass

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+/**
+ * (c) 2015 Ruben Schmidmeister. <ruby@fog.im>
+ */
+
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('autoprefixer'),
+    postcss = require('gulp-postcss');
+
+
+gulp.task('build', function () {
+    gulp.src('sass/style.scss')
+        .pipe(sass())
+        .pipe(postcss([autoprefixer({ cascade: false })]))
+        .pipe(gulp.dest('css/'));
+});
+
+gulp.task('watch', function(){
+   gulp.watch('sass/**/*.scss', ['build']);
+});
