425012f65cda3b89a274a90b9e9e451fefbbc809
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-05-07 16:12:08 +0200

    remove js, add deploy script

--- js/src/main.js
+++
@@ -1,5 +0,0 @@
-/**
- * (c) 2016 Ruben Schmidmeister <ruby@fog.im>
- */
-
-console.log('hello world')
---
+++ scripts/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+FILES=${@:1}
+CURRENT_DIRECTORY=`pwd`
+TEMP_DIR=$(mktemp -d)
+REMOTE_URL=$(git remote get-url --push origin)
+BRANCH="master"
+
+cleanup() {
+    rm -rf ${TEMP_DIR}
+    cd ${CURRENT_DIRECTORY}
+}
+
+trap cleanup EXIT
+
+echo "Cloning repository..."
+git clone -b ${BRANCH} $(pwd) ${TEMP_DIR} -q
+
+echo "Cleaning old files..."
+
+OLD_FILES=$(find ${TEMP_DIR} -maxdepth 1 -mindepth 1 -not -name ".git")
+
+for FILE in ${OLD_FILES}; do
+    echo "removing ${FILE}"
+    rm -rf ${FILE}
+done
+
+echo "Copying new files..."
+
+for FILE in ${FILES}; do
+    cp -R ${FILE} ${TEMP_DIR}/
+done
+
+cd ${TEMP_DIR}
+git add -A
+git status
