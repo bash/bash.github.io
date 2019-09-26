fdfbdc35cfeee3151cb137deff603b11b307ea4e
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2019-09-26 20:06:19 +0200

    Print contents of build dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,3 +8,5 @@ mkdir -p -- "$build_directory"
 
 cp -R icons CNAME index.html style.css \
       "$build_directory"
+
+find -mindepth 1 "$build_directory"
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ set -o xtrace
 dpl pages git \
     --no-committer_from_gh \
     --target_branch master \
-    --local_dir build \
+    --local_dir ./build \
     --token "$GITHUB_TOKEN" \
     --keep-history \
     --allow_empty_commit \
