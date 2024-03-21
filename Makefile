2483fa487355400cac132fb07411d0ccf8847c34
Tau <git@tau.garden>
2024-03-21 20:11:57 +0100

    Use lightning css to minify css

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
+MINIFY := npx lightningcss --minify --browserslist
+
 .PHONY: run build
 
 run:
 	zola serve --drafts
 
 build:
-	python ./update-reading-list.py
-	./fetch-katex.sh
-	zola build --output-dir _site
+	./make-scripts/build.sh
---
+++ make-scripts/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function minify() {
+    dos2unix -r "$1"
+    npx lightningcss --minify --browserslist "$1" -o "$1"
+}
+
+python ./update-reading-list.py
+./fetch-katex.sh
+npm clean-install
+
+zola build --output-dir _site --force
+
+find _site -name '*.css' \
+    -exec dos2unix -r {} \; \
+    -exec npx lightningcss --minify --error-recovery --browserslist {} -o {} \;
