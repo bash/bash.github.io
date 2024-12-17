68cc75a2c9a7bc66b01ab1b88c43f0f7e7d1653a
Tau Gärtli <git@tau.garden>
2024-12-17 12:47:53 +0100

    Pass all css files to dos2unix at once

--- make-scripts/minify-css.bash
+++ make-scripts/minify-css.bash
@@ -2,6 +2,5 @@
 
 set -euo pipefail
 
-find "$1" -name '*.css' \
-    -exec dos2unix -r {} \; \
-    -exec npx lightningcss --minify --error-recovery --browserslist {} -o {} \;
+find "$1" -name '*.css' -exec dos2unix -r {} +
+find "$1" -name '*.css' -exec npx lightningcss --minify --error-recovery --browserslist {} -o {} \;
